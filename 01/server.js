5448dffdad2ac10f95e67e36e2471033f73abfc6
drozdovalena <drozdova.el.gen@gmail.com>
2024-01-31 21:26:44 +0100

    hw video05

--- 01/server.js
+++ 01/server.js
@@ -3,8 +3,25 @@ const http = require('http')
 let requestsCount = 0
 
 const server = http.createServer((request, response)=> {
-    requestsCount++
-    response.write('IBlaBlabla ' + requestsCount);
+
+    switch (request.url){
+        case '/favicon.ico':
+            break;
+        case '/students':
+            response.write('Students');
+            requestsCount++
+            break;
+        case '/courses':
+            response.write('FONT + BACK')
+            requestsCount++
+            break;
+        default:
+            response.write('404')
+            requestsCount++
+
+
+    }
+    response.write(' IBlaBlabla ' + requestsCount);
     response.end()
 })
 
